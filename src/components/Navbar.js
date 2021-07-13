4c5ca8bfa812312d3d6b035396455ce08d6ae19f
Shivam Sharma <shivam01912@gmail.com>
2021-07-13 13:57:49 +0530

    added favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#experiences" className="mr-5 hover:text-white">
             Past Experience
           </a>
           <a href="#skills" className="mr-5 hover:text-white">
