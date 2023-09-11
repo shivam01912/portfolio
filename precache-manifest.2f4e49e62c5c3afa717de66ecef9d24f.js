968ee2fef7896cc7949f1235c940d6ccda8aea94
Shivam <shivam_sharma@intuit.com>
2023-09-11 11:59:29 +0530

    Updates

---
+++ precache-manifest.2f4e49e62c5c3afa717de66ecef9d24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a339466250b1cad7d2cf",
+    "url": "/portfolio/static/css/main.85785a78.chunk.css"
+  },
+  {
+    "revision": "a339466250b1cad7d2cf",
+    "url": "/portfolio/static/js/main.a3394662.chunk.js"
+  },
+  {
+    "revision": "fbd5260c374b1f5d327f",
+    "url": "/portfolio/static/css/1.79cec37d.chunk.css"
+  },
+  {
+    "revision": "fbd5260c374b1f5d327f",
+    "url": "/portfolio/static/js/1.fbd5260c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "b3ec3944fe5368fedf4c8870abdf29e8",
+    "url": "/portfolio/index.html"
+  }
+];
